3f7c76ac2dfeba1edcaf60163578b2ce1b2cd78e
Natanael Forte <natanaelforte@gmail.com>
2023-03-30 14:40:20 -0300

    botoes_na_row

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,42 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      body: Column(children: const [
-        Text(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children:  [
+        const Text(
           'Pode Entrar!',
-          style: TextStyle(fontSize: 30),
-        )
-      ]),
-    ));
+          style: TextStyle(fontSize: 30),  
+        ),
+        const Text('0', style: TextStyle(fontSize: 100),),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {},
+              style:const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(Colors.red), fixedSize: MaterialStatePropertyAll(Size.fromWidth(150))
+                ), 
+              child: const Text(
+                'Entrou',
+                style: TextStyle(color: Colors.black, fontSize: 25)
+               ),
+              ),
+            TextButton(
+              onPressed: () {},
+              style:const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(Colors.red),fixedSize: MaterialStatePropertyAll(Size.fromWidth(150))
+                ), 
+              child: const Text(
+                'Saiu',
+                style: TextStyle(color: Colors.black, fontSize: 25)
+               ),
+              ),
+             ],
+            ),
+          ]
+        ),
+      )
+    );
   }
 }
